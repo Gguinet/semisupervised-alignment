2188bd1ad5c5f675501d709fd63295a097aeb43a
Gguinet <gauthier.guinet@polytechnique.edu>
2020-03-29 22:41:34 +0200

    reverse computation

--- lang_variation.sh
+++ lang_variation.sh
@@ -6,86 +6,86 @@ echo "EN-ES, EN-FR, EN-DE, EN-RU Induction using all available languages"
 echo "Danish Pivot"
 
 #Danish: da
-sh bli.sh en da es
+sh bli.sh en es da
 
 #Danish: da
-sh bli.sh en da fr
+sh bli.sh en fr da
 
 #Danish: da
-sh bli.sh en da de
+sh bli.sh en de da
 
 #Danish: da
-sh bli.sh en da ru
+sh bli.sh en ru da
 
 echo "Dutch Pivot"
 
 #Dutch: nl
-sh bli.sh en nl es
+sh bli.sh en es nl
 
 #Dutch: nl
-sh bli.sh en nl fr
+sh bli.sh en fr nl
 
 #Dutch: nl
-sh bli.sh en nl de
+sh bli.sh en de nl
 
 #Dutch: nl
-sh bli.sh en nl ru
+sh bli.sh en ru nl
 
 echo "French Pivot"
 
 #French: fr
-sh bli.sh en fr es
+#sh bli.sh en fr es
 
 #French: fr
 #sh bli.sh en fr fr
 
 #French: fr
-sh bli.sh en fr de
+#sh bli.sh en fr de
 
 #French: fr
-sh bli.sh en fr ru
+#sh bli.sh en fr ru
 
 echo "German Pivot"
 
 #German: de
-sh bli.sh en de es
+#sh bli.sh en de es
 
 #German: de
-sh bli.sh en de fr
+#sh bli.sh en de fr
 
 #German: de
 #sh bli.sh en de de
 
 #German: de
-sh bli.sh en de ru
+#sh bli.sh en de ru
 
 echo "Italian Pivot"
 
 #Italian: it
-sh bli.sh en it es
+sh bli.sh en es it
 
 #Italian: it
-sh bli.sh en it fr
+sh bli.sh en fr it
 
 #Italian: it
-sh bli.sh en it de
+sh bli.sh en de it
 
 #Italian: it
-sh bli.sh en it ru
+sh bli.sh en ru it
 
 echo "Portuguese Pivot"
 
 #Portuguese: pt
-sh bli.sh en pt es
+sh bli.sh en es pt
 
 #Portuguese: pt
-sh bli.sh en pt fr
+sh bli.sh en fr pt
 
 #Portuguese: pt
-sh bli.sh en pt de
+sh bli.sh en de pt
 
 #Portuguese: pt
-sh bli.sh en pt ru
+sh bli.sh en ru pt
 
 echo "Spanish Pivot"
 
@@ -93,332 +93,333 @@ echo "Spanish Pivot"
 #sh bli.sh en es es
 
 #Spanish: es
-sh bli.sh en es fr
+#sh bli.sh en es fr
 
 #Spanish: es
-sh bli.sh en es de
+#sh bli.sh en es de
 
 #Spanish: es
-sh bli.sh en es ru
+#sh bli.sh en es ru
 
 echo "Russian Pivot"
 
 #Russian: ru
-sh bli.sh en ru es
+#sh bli.sh en ru es
 
 #Russian: ru
-sh bli.sh en ru fr
+#sh bli.sh en ru fr
 
 #Russian: ru
-sh bli.sh en ru de
+#sh bli.sh en ru de
 
 #Russian: ru
 #sh bli.sh en ru ru
 
-echo "Slovak Pivot"
+echo "Slovak Pivot Weak"
 
 #Slovak: sk
-sh bli.sh en sk es
+#sh bli.sh en sk es
 
 #Slovak: sk
-sh bli.sh en sk fr
+#sh bli.sh en sk fr
 
 #Slovak: sk
-sh bli.sh en sk de
+#sh bli.sh en sk de
 
 #Slovak: sk
-sh bli.sh en sk ru
+#sh bli.sh en sk ru
 
 echo "Ukrainian"
 
 #Ukrainian: uk
-sh bli.sh en uk es
+sh bli.sh en es uk
 
 #Ukrainian: uk
-sh bli.sh en uk fr
+sh bli.sh en fr uk
 
 #Ukrainian: uk
-sh bli.sh en uk de
+sh bli.sh en de uk
 
 #Ukrainian: uk
-sh bli.sh en uk ru
+sh bli.sh en ru uk
 
 echo "Arabic Pivot"
 
 #Arabic (ar)
-sh bli.sh en ar es
+sh bli.sh en es ar
 
 #Arabic (ar)
-sh bli.sh en ar fr
+sh bli.sh en fr ar
 
 #Arabic (ar)
-sh bli.sh en ar de
+sh bli.sh en de ar
 
 #Arabic (ar)
-sh bli.sh en ar ru
+sh bli.sh en ru ar
 
 echo "Bulgarian Pivot"
 
 #Bulgarian: bg
-sh bli.sh en bg es
+sh bli.sh en es bg
 
 #Bulgarian: bg
-sh bli.sh en bg fr
+sh bli.sh en fr bg
 
 #Bulgarian: bg
-sh bli.sh en bg de
+sh bli.sh en de bg
 
 #Bulgarian: bg
-sh bli.sh en bg ru
+sh bli.sh en ru bg
 
 echo "Catalan Pivot"
 
 #Catalan: ca
-sh bli.sh en ca es
+sh bli.sh en es ca
 
 #Catalan: ca
-sh bli.sh en ca fr
+sh bli.sh en fr ca
 
 #Catalan: ca
-sh bli.sh en ca de
+sh bli.sh en de ca
 
 #Catalan: ca
-sh bli.sh en ca ru
+sh bli.sh en ru ca
 
 echo "Croatian Pivot"
 
 #Croatian: hr
-sh bli.sh en hr es
+sh bli.sh en es hr
 
 #Croatian: hr
-sh bli.sh en hr fr
+sh bli.sh en fr hr
 
 #Croatian: hr
-sh bli.sh en hr de
+sh bli.sh en de hr
 
 #Croatian: hr
-sh bli.sh en hr ru
+sh bli.sh en ru hr
 
 echo "Czech Pivot"
 
 #Czech: cs
-sh bli.sh en cs es
+sh bli.sh en es cs
 
 #Czech: cs
-sh bli.sh en cs fr
+sh bli.sh en fr cs
 
 #Czech: cs
-sh bli.sh en cs de
+sh bli.sh en de cs
 
 #Czech: cs
-sh bli.sh en cs ru
+sh bli.sh en ru cs
 
-echo "Estonian Pivot"
-
-#Estonian: et
-sh bli.sh en et es
+echo "Estonian Pivot Weak"
 
 #Estonian: et
-sh bli.sh en et fr
+#sh bli.sh en et es
 
 #Estonian: et
-sh bli.sh en et de
+#sh bli.sh en et fr
 
 #Estonian: et
-sh bli.sh en et ru
+#sh bli.sh en et de
+
+#Estonian: et
+#sh bli.sh en et ru
 
 echo "Greek Pivot"
 
 #Greek: el
-sh bli.sh en el es
+sh bli.sh en es el
 
 #Greek: el
-sh bli.sh en el fr
+sh bli.sh en fr el
 
 #Greek: el
-sh bli.sh en el de
+sh bli.sh en de el
 
 #Greek: el
-sh bli.sh en el ru
+sh bli.sh en ru el
 
 echo "Hebrew Pivot"
 
 #Hebrew: he
-sh bli.sh en he es
+sh bli.sh en es he
 
 #Hebrew: he
-sh bli.sh en he fr
+sh bli.sh en fr he
 
 #Hebrew: he
-sh bli.sh en he de
+sh bli.sh en de he
 
 #Hebrew: he
-sh bli.sh en he ru
+sh bli.sh en ru he
 
 echo "Hungarian Pivot"
 
 #Hungarian: hu
-sh bli.sh en hu es
+sh bli.sh en es hu
 
 #Hungarian: hu
-sh bli.sh en hu fr
+sh bli.sh en fr hu
 
 #Hungarian: hu
-sh bli.sh en hu de
+sh bli.sh en de hu
 
 #Hungarian: hu
-sh bli.sh en hu ru
+sh bli.sh en ru hu
 
 echo "Indonesian Pivot"
 
 #Indonesian: id
-sh bli.sh en id es
+sh bli.sh en es id
 
 #Indonesian: id
-sh bli.sh en id fr
+sh bli.sh en fr id
 
 #Indonesian: id
-sh bli.sh en id de
+sh bli.sh en de id
 
 #Indonesian: id
-sh bli.sh en id ru
+sh bli.sh en ru id
 
 echo "Macedonian Pivot"
 
 #Macedonian: mk
-sh bli.sh en mk es
+#sh bli.sh en mk es
 
 #Macedonian: mk
-sh bli.sh en mk fr
+#sh bli.sh en mk fr
 
 #Macedonian: mk
-sh bli.sh en mk de
+#sh bli.sh en mk de
 
 #Macedonian: mk
-sh bli.sh en mk ru
+#sh bli.sh en mk ru
 
 echo "Norwegian Pivot"
 
 #Norwegian: no
-sh bli.sh en no es
+sh bli.sh en es no
 
 #Norwegian: no
-sh bli.sh en no fr
+sh bli.sh en fr no
 
 #Norwegian: no
-sh bli.sh en no de
+sh bli.sh en de no
 
 #Norwegian: no
-sh bli.sh en no ru
+sh bli.sh en ru no
 
 echo "Polish Pivot"
 
 #Polish: pl
-sh bli.sh en pl es
+sh bli.sh en es pl
 
 #Polish: pl
-sh bli.sh en pl fr
+sh bli.sh en fr pl
 
 #Polish: pl
-sh bli.sh en pl de
+sh bli.sh en de pl
 
 #Polish: pl
-sh bli.sh en pl ru
+sh bli.sh en ru pl
 
 echo "Romanian Pivot"
 
 #Romanian: ro
-sh bli.sh en ro es
+sh bli.sh en es ro
 
 #Romanian: ro
-sh bli.sh en ro fr
+sh bli.sh en fr ro
 
 #Romanian: ro
-sh bli.sh en ro de
+sh bli.sh en de ro
 
 #Romanian: ro
-sh bli.sh en ro ru
+sh bli.sh en ru ro
 
 echo "Slovenian Pivot"
 
 #Slovenian: sl
-sh bli.sh en sl es
+sh bli.sh en es sl
 
 #Slovenian: sl
-sh bli.sh en sl fr
+sh bli.sh en fr sl
 
 #Slovenian: sl
-sh bli.sh en sl de
+sh bli.sh en de sl
 
 #Slovenian: sl
-sh bli.sh en sl ru
+sh bli.sh en ru sl
 
 echo "Swedish Pivot"
 
 #Swedish: sv
-sh bli.sh en sv es
+sh bli.sh en es sv
 
 #Swedish: sv
-sh bli.sh en sv fr
+sh bli.sh en fr sv
 
 #Swedish: sv
-sh bli.sh en sv de
+sh bli.sh en de sv
 
 #Swedish: sv
-sh bli.sh en sv ru
+sh bli.sh en ru sv
 
 echo "Turkish Pivot"
 
 #Turkish: tr
-sh bli.sh en tr es
+sh bli.sh en es tr
 
 #Turkish: tr
-sh bli.sh en tr fr
+sh bli.sh en fr tr
 
 #Turkish: tr
-sh bli.sh en tr de
+sh bli.sh en de tr
 
 #Turkish: tr
-sh bli.sh en tr ru
+sh bli.sh en ru tr
 
 echo "Ukrainian Pivot"
 
 #Ukrainian: uk
-sh bli.sh en uk es
+sh bli.sh en es uk
 
 #Ukrainian: uk
-sh bli.sh en uk fr
+sh bli.sh en fr uk
 
 #Ukrainian: uk
-sh bli.sh en uk de
+sh bli.sh en de uk
 
 #Ukrainian: uk
-sh bli.sh en uk ru
+sh bli.sh en ru uk
 
 echo "Vietnamese Pivot"
 
 #Vietnamese: vi
-sh bli.sh en "vi" es
+#sh bli.sh en "vi" es
 
 #Vietnamese: vi
-sh bli.sh en "vi" fr
+#sh bli.sh en "vi" fr
 
 #Vietnamese: vi
-sh bli.sh en "vi" de
+#sh bli.sh en "vi" de
 
 #Vietnamese: vi
-sh bli.sh en "vi" ru
+#sh bli.sh en "vi" ru
 
 echo "Finnish Pivot"
 
 #Finnish: fi
-sh bli.sh en "fi" es
+#sh bli.sh en "fi" es
 
 #Finnish: fi
-sh bli.sh en "fi" fr
+#sh bli.sh en "fi" fr
 
 #Finnish: fi
-sh bli.sh en "fi" de
+#sh bli.sh en "fi" de
 
 #Finnish: fi
-sh bli.sh en "fi" ru
+#sh bli.sh en "fi" ru
+
